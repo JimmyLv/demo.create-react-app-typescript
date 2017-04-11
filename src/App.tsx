3697314569cc687ae0400c0772140648db6fd34a
Jimmy Lv <jimmy.jinglv@gmail.com>
2017-04-11 22:38:43 +1000

    [#1] - Fixed all tslint error and refactored to use styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,51 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
+import styled, { keyframes } from 'styled-components'
 
-const logo = require('./logo.svg');
+const logo = require('./logo.svg')
 
-class App extends React.Component<{}, {}> {
+declare interface WrapperProps {
+  center: boolean
+}
+
+const Wrapper = styled.div`
+  text-align: ${(props: WrapperProps) => props.center ? 'center' : 'left'};
+`
+
+const Header = styled.div`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`
+
+const spin = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`
+
+const Logo = styled.img`
+  animation: ${spin} infinite 20s linear;
+  height: 80px;
+`
+
+const Intro = styled.p`
+  font-size: large;
+`
+
+class App extends React.PureComponent<{}, {}> {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <Wrapper center={true}>
+        <Header>
+          <Logo src={logo} alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
+        </Header>
+        <Intro>
           To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
+        </Intro>
+      </Wrapper>
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { injectGlobal } from 'styled-components'
+
+import App from './App'
+
+/* tslint:disable */
+injectGlobal`body {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+`
+/* tslint:enable */
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
-);
+)
