11c2e1ef641a31cefb40bf220812d2c640204b09
Jimmy Lv <jimmy.jinglv@gmail.com>
2017-04-22 19:16:52 +1000

    [#10] - Finished users model with fetch reducer and related effort, also make request when enter 'users' pathname

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,8 +1,58 @@
-import { Model } from 'dva'
+import { EffectsCommandMap, Model, SubscriptionAPI } from 'dva'
+import { FSA } from 'flux-standard-action'
+import { retrieve } from '../service/users'
+import { Location } from 'history'
 
-const users: Model =  {
+interface User {
+  id: number
+  name: string
+  username: string
+  email: string
+  website: string
+}
+
+export declare interface UsersState {
+  list: Array<User>
+  total: number
+  page: number
+}
+
+const users: Model = {
   namespace: 'users',
-  state: [1, 2]
+  state: {
+    list: [],
+    total: null,
+    page: null
+  },
+  reducers: {
+    save(state: UsersState, action: FSA<{ data: Array<User>, page: number, total: number }, {}>) {
+      const { payload: { data: list, total, page } } = action
+      return { ...state, list, total, page }
+    }
+  },
+  effects: {
+    *fetch({ payload: { page = 1 } }: FSA<{ page: number }, {}>, { call, put }: EffectsCommandMap) {
+      const { data, headers } = yield call(retrieve, { page })
+      yield put({
+        type: 'save',
+        payload: {
+          data,
+          total: parseInt(headers['x-total-count'], 10),
+          page
+        }
+      })
+    }
+  },
+  subscriptions: {
+    setup({ dispatch, history }: SubscriptionAPI) {
+      return history.listen((location: Location) => {
+        const { pathname, query } = location
+        if (pathname === '/users') {
+          dispatch({ type: 'fetch', payload: query })
+        }
+      })
+    }
+  }
 }
 
 export default users
--- src/service/users.ts
+++ src/service/users.ts
@@ -1,6 +1,6 @@
 import request from '../utils/request'
 import { PAGE_SIZE } from '../constants'
 
-export function fetch({ page = 1 }: { page: number }) {
+export function retrieve({ page = 1 }: { page: number }) {
   return request(`/api/users?_page=${page}&_limit=${PAGE_SIZE}`)
 }
